9f06b675da6f869194b4939ab7bba8277f06f3c8
Stephen Carey <Stephen.Carey@sqs.com>
2016-07-11 16:12:56 +0100

    Adding new security paths.

--- src/main/java/org/bhc/WebSecurityConfiguration.java
+++ src/main/java/org/bhc/WebSecurityConfiguration.java
@@ -1,6 +1,8 @@
 package org.bhc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,7 +17,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                    .antMatchers("/").permitAll()
+                    .antMatchers("/",
+                            "/membership",
+                            "/infoAndRules",
+                            "/calendar",
+                            "/beginners",
+                            "/css/**",
+                            "/fonts/**",
+                            "/js/**",
+                            "/img/**",
+                            "favicon.ico").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -24,5 +35,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .logoutSuccessUrl("/").permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 }
