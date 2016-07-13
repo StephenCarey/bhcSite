8441a8996a6d5dbaa2c68fa883de577711c0ecdb
Stephen Carey <Stephen.Carey@sqs.com>
2016-07-13 14:39:51 +0100

    Adding create and insert SQL as well as models and repository classes

--- src/main/java/org/bhc/persistance/models/Walk.java
+++ src/main/java/org/bhc/persistance/models/Walk.java
@@ -1,23 +1,71 @@
 package org.bhc.persistance.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 public class Walk {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    public String name;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
     private String description;
+
+    @Column(name = "WALKDATE")
     private Date walkDate;
+
+    public Walk() {
+
+    }
 
     public Walk(String name, String description){
         this.name = name;
         this.description = description;
+        this.walkDate = new Date();
+    }
+
+
+    public Walk(String name, String description, Date walkDate){
+        this.name = name;
+        this.description = description;
+        this.walkDate = walkDate;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getWalkDate() {
+        return walkDate;
+    }
+
+    public void setWalkDate(Date walkDate) {
+        this.walkDate = walkDate;
     }
 }
--- src/main/java/org/bhc/persistance/repository/WalkRepository.java
+++ src/main/java/org/bhc/persistance/repository/WalkRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-
 public interface  WalkRepository  extends JpaRepository<Walk, Long> {
 
     List<Walk> findByName(String name);
